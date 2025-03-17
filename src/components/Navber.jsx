ee48018c24637be713cb754669491c78e9476d88
sadik126 <sadikhimel04@gmail.com>
2025-03-17 21:41:02 +0600

    Refactor PostCSS configuration, update dependencies, add navigation component, and include new SVG icons

---
+++ src/components/Navber.jsx
@@ -0,0 +1,53 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+
+export default function Navber() {
+    const navmenu = () => {
+        return (
+            <>
+                <li><Link>Home</Link></li>
+
+                <li><Link>About</Link></li>
+                <li><Link>Services</Link></li>
+                <li><Link>Blog</Link></li>
+                <li><Link>Contact</Link></li>
+
+
+            </>
+        )
+    }
+    return (
+        <div className="navbar bg-base-100 shadow-sm">
+            <div className="navbar-start">
+                <div className="dropdown">
+                    <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
+                    </div>
+                    <ul
+                        tabIndex={0}
+                        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+                        {
+                            navmenu()
+                        }
+
+                    </ul>
+                </div>
+                <Link href='/' className="btn btn-ghost text-xl">
+                    {/* <img /> */}
+                    <Image src={'/assets/logo.svg'} width={50} height={50} alt='logo' />
+                </Link>
+            </div>
+            <div className="navbar-center hidden lg:flex">
+                <ul className="menu menu-horizontal px-1">
+                    {
+                        navmenu()
+                    }
+                </ul>
+            </div>
+            <div className="navbar-end">
+                <a className="btn">Button</a>
+            </div>
+        </div>
+    )
+}
